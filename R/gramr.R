43753f6835d75f24efbdb500a86a23060031d40e
jasdumas <jasmine.dumas@gmail.com>
2017-05-25 16:33:10 -0700

    add package level documentation

--- R/gramr.R
+++ R/gramr.R
@@ -1,52 +1,8 @@
-#' <Add Title>
+#' A R package To Check for Grammar within RMarkdown documents.
+#' @description Provides additional checks in grammar not currently caught
+#' by the native spell check within RStudio.
 #'
-#' <Add Description>
-#'
-#' @import htmlwidgets
-#'
-#' @export
-gramr <- function(message, width = NULL, height = NULL, elementId = NULL) {
+#' @docType package
+#' @name gramr
+NULL
 
-  # forward options using x
-  x = list(
-    message = message
-  )
-
-  # create widget
-  htmlwidgets::createWidget(
-    name = 'gramr',
-    x,
-    width = width,
-    height = height,
-    package = 'gramr',
-    elementId = elementId
-  )
-}
-
-#' Shiny bindings for gramr
-#'
-#' Output and render functions for using gramr within Shiny
-#' applications and interactive Rmd documents.
-#'
-#' @param outputId output variable to read from
-#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
-#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
-#'   string and have \code{'px'} appended.
-#' @param expr An expression that generates a gramr
-#' @param env The environment in which to evaluate \code{expr}.
-#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
-#'   is useful if you want to save an expression in a variable.
-#'
-#' @name gramr-shiny
-#'
-#' @export
-gramrOutput <- function(outputId, width = '100%', height = '400px'){
-  htmlwidgets::shinyWidgetOutput(outputId, 'gramr', width, height, package = 'gramr')
-}
-
-#' @rdname gramr-shiny
-#' @export
-renderGramr <- function(expr, env = parent.frame(), quoted = FALSE) {
-  if (!quoted) { expr <- substitute(expr) } # force quoted
-  htmlwidgets::shinyRenderWidget(expr, gramrOutput, env, quoted = TRUE)
-}
