b0a3a5b753966a277667e08e034861aee303f6b2
jasdumas <jasmine.dumas@gmail.com>
2017-05-25 16:27:14 -0700

    remove files and add some attempts for htmlwidgets, but def not using htmlwidgets

---
+++ R/write_good.R
@@ -0,0 +1,12 @@
+#' Detecting programming nomenclature
+#'
+#' @param data a markdown document or sentence to examine
+#' @param ...
+#' @return
+#' @export
+#'
+#' @examples
+write_good <- function(data,  ...){
+  write_good <- list()
+}
+
